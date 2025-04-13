89a85eb2a1faa7b2e49f4bc2743bc8829f4e8b75
MarlonSantos321 <202407257119@alunos.estacio.br>
2025-04-13 20:21:55 -0300

    Add files via upload

---
+++ xadrez novato.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // Movimentação da Torre (5 casas para a direita)
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+
+    // Movimentação do Bispo (5 casas na diagonal para cima e à direita)
+    printf("\nMovimento do Bispo:\n");
+    int i = 0;
+    while (i < 5) {
+        printf("Cima\n");
+        printf("Direita\n");
+        i++;
+    }
+
+    // Movimentação da Rainha (8 casas para a esquerda)
+    printf("\nMovimento da Rainha:\n");
+    int j = 0;
+    do {
+        printf("Esquerda\n");
+        j++;
+    } while (j < 8);
+
+    return 0;
+}
